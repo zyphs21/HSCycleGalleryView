09e56f62f62e272e5deddd10e46ab1317279b67d
zyphs21 <hansenhs21@live.com>
2018-01-16 14:21:54 +0800

    set up project

---
+++ HSCycleGalleryView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HSCycleGalleryView
+//
+//  Created by Hanson on 2018/1/16.
+//  Copyright © 2018年 HansonStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
