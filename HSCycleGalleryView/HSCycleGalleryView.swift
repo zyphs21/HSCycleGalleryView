b333ffe2093f1fdf6837a192ff6bb219e5530a6a
zyphs21 <hansenhs21@live.com>
2018-01-16 14:32:47 +0800

    add HSCycleGalleryView and HSCycleGalleryViewLayout

---
+++ HSCycleGalleryView/HSCycleGalleryView.swift
@@ -0,0 +1,179 @@
+//
+//  HSCycleGalleryView.swift
+//  HSCycleGalleryView
+//
+//  Created by Hanson on 2018/1/16.
+//  Copyright © 2018年 HansonStudio. All rights reserved.
+//
+
+import UIKit
+
+@objc protocol CycleGalleryViewDelegate: class {
+    
+    func numberOfItemInCycleGalleryView(_ cycleGalleryView: HSCycleGalleryView) -> Int
+    
+    func cycleGalleryView(_ cycleGalleryView: HSCycleGalleryView, cellForItemAtIndex index: Int) -> UICollectionViewCell
+    
+    @objc optional func cycleGalleryView(_ cycleGalleryView: HSCycleGalleryView, didSelectItemCell cell: UICollectionViewCell, at Index: Int)
+}
+
+class HSCycleGalleryView: UIView {
+    
+    weak var delegate: CycleGalleryViewDelegate?
+    
+    var collectionView: UICollectionView!
+    private let groupCount = 200
+    private var indexArr = [Int]()
+    var dataNum: Int = 0
+    
+    var autoScrollInterval: Double = 3
+    
+    var timer: Timer?
+    var currentIndexPath: IndexPath!
+    
+    
+    // MARK: - Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        collectionView = UICollectionView(frame: frame, collectionViewLayout: HSCycleGalleryViewLayout())
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.backgroundColor = UIColor.white
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        self.addSubview(collectionView)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func willMove(toSuperview newSuperview: UIView?) {
+        if newSuperview != nil {
+            self.removeTimer()
+            if self.autoScrollInterval > 0 {
+                self.addTimer()
+            }
+        } else {
+            self.removeTimer()
+        }
+    }
+}
+
+
+// MARK: - Public Function
+
+extension HSCycleGalleryView {
+    
+    func reloadData() {
+        guard let dataNum = delegate?.numberOfItemInCycleGalleryView(self) else { return }
+        self.dataNum = dataNum
+        indexArr.removeAll()
+        for _ in 0 ..< groupCount {
+            for j in 0 ..< dataNum {
+                indexArr.append(j)
+            }
+        }
+        collectionView.reloadData()
+        // Scroll to the middle
+        currentIndexPath = IndexPath(item: groupCount / 2 * dataNum, section: 0)
+        collectionView.scrollToItem(at: currentIndexPath, at: .centeredHorizontally, animated: false)
+    }
+    
+    func register(_ cellClass: AnyClass?, forCellReuseIdentifier identifier: String) {
+        self.collectionView.register(cellClass, forCellWithReuseIdentifier: identifier)
+    }
+    
+    func register(_ nib: UINib?, forCellReuseIdentifier identifier: String) {
+        self.collectionView.register(nib, forCellWithReuseIdentifier: identifier)
+    }
+    
+    func dequeueReusableCell(withIdentifier identifier: String, for indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath)
+        
+        return cell
+    }
+}
+
+
+// MARK: - Private Function
+
+extension HSCycleGalleryView {
+    
+    func removeTimer() {
+        self.timer?.invalidate()
+        timer = nil
+    }
+    
+    func addTimer() {
+        if timer != nil {
+            return
+        }
+        timer = Timer(timeInterval: autoScrollInterval, target: self, selector: #selector(autoScroll), userInfo: nil, repeats: true)
+        RunLoop.main.add(timer!, forMode: .commonModes)
+        //        timer = Timer.scheduledTimer(timeInterval: TimeInterval(autoScrollInterval), target: self, selector: #selector(autoScroll), userInfo: nil, repeats: true)
+    }
+    
+    @objc func autoScroll() {
+        if self.superview == nil || self.window == nil {
+            return
+        }
+        print("timer-autoScroll")
+        self.scrollToNextIndex()
+    }
+    
+    private func scrollToNextIndex() {
+        var currentIndex = currentIndexPath.row
+        if (currentIndex + 1) >= indexArr.count {
+            currentIndex = groupCount / 2 * self.dataNum
+            collectionView.scrollToItem(at: IndexPath(item: currentIndex, section: 0), at: .centeredHorizontally, animated: false)
+        }
+        let nextIndex = currentIndex + 1
+        collectionView.scrollToItem(at: IndexPath(item: nextIndex, section: 0), at: .centeredHorizontally, animated: true)
+        currentIndexPath = IndexPath(item: nextIndex, section: 0)
+    }
+}
+
+extension HSCycleGalleryView: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return indexArr.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard indexArr.count > 0 else { return UICollectionViewCell() }
+        let index = indexArr[indexPath.row]
+        let dataIndex = index % dataNum
+        
+        return self.delegate?.cycleGalleryView(self, cellForItemAtIndex: dataIndex) ?? UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let index = indexArr[indexPath.row]
+        let dataIndex = index % dataNum
+        let cell = collectionView.cellForItem(at: indexPath) ?? UICollectionViewCell()
+        
+        delegate?.cycleGalleryView?(self, didSelectItemCell: cell, at: dataIndex)
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let pointInView = self.convert(collectionView.center, to: collectionView)
+        let indexPathNow = collectionView.indexPathForItem(at: pointInView)
+        let index = (indexPathNow?.row ?? 0) % dataNum
+        // animate stop , locate to the middle
+        currentIndexPath = IndexPath(item: groupCount / 2 * dataNum + index, section: 0)
+        collectionView.scrollToItem(at: currentIndexPath, at: .centeredHorizontally, animated: false)
+    }
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        addTimer()
+    }
+    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        removeTimer()
+    }
+}
+
---
+++ HSCycleGalleryView/HSCycleGalleryViewLayout.swift
@@ -0,0 +1,81 @@
+//
+//  HSCycleGalleryViewLayout.swift
+//  HSCycleGalleryView
+//
+//  Created by Hanson on 2018/1/16.
+//  Copyright © 2018年 HansonStudio. All rights reserved.
+//
+
+import UIKit
+
+class HSCycleGalleryViewLayout: UICollectionViewFlowLayout {
+    
+    var itemWidth: CGFloat = 290
+    var itemHeight: CGFloat = 133
+    
+    override func prepare() {
+        super.prepare()
+        
+        self.itemSize = CGSize(width: itemWidth, height: itemHeight)
+        self.scrollDirection = .horizontal
+        // 下面 layoutAttributesForElements 中将item scale变换后，自然有空隙了
+        self.minimumInteritemSpacing = 0
+        self.minimumLineSpacing = 0
+        
+        // 为了让第一张图片与最后一张图片出现在最中央
+        let left = (self.collectionView!.bounds.width - itemWidth) / 2
+        let top = (self.collectionView!.bounds.height - itemHeight) / 2
+        self.sectionInset = UIEdgeInsetsMake(top, left, top, left)
+        
+    }
+    
+    override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
+        return true
+    }
+    
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        let array = super.layoutAttributesForElements(in: rect)
+        let visiableRect = CGRect(x: self.collectionView!.contentOffset.x,
+                                  y: self.collectionView!.contentOffset.y,
+                                  width: self.collectionView!.frame.width,
+                                  height: self.collectionView!.frame.height)
+        
+        // 当前屏幕中点，相对于collect view上的x坐标
+        let centerX = self.collectionView!.contentOffset.x + self.collectionView!.bounds.width / 2
+        
+        for attributes in array! {
+            if !visiableRect.intersects(attributes.frame) { continue }
+            
+            let offsetCenterX = abs(attributes.center.x - centerX)
+            let scale = max(1 - offsetCenterX / self.collectionView!.bounds.width * 0.4, 0.8)
+            attributes.transform = CGAffineTransform(scaleX: scale, y: scale)
+        }
+        
+        return array
+    }
+    
+    /// 用来设置collectionView停止滚动那一刻的位置(实现目的是当停止滑动，时刻有一张图片是位于屏幕最中央的)
+    override func targetContentOffset(forProposedContentOffset
+        proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
+        let lastRect = CGRect(x: proposedContentOffset.x, y: proposedContentOffset.y,
+                              width: self.collectionView!.bounds.width,
+                              height: self.collectionView!.bounds.height)
+        // 当前屏幕中点，相对于collect view上的x坐标
+        let centerX = proposedContentOffset.x + self.collectionView!.bounds.width * 0.5;
+        let array = self.layoutAttributesForElements(in: lastRect)
+        
+        //需要移动的距离
+        var adjustOffsetX = CGFloat(MAXFLOAT);
+        for attri in array! {
+            //每个单元格里中点的偏移量
+            let deviation = attri.center.x - centerX
+            //保存偏移最小的那个
+            if abs(deviation) < abs(adjustOffsetX) {
+                adjustOffsetX = deviation
+            }
+        }
+        // 通过偏移量返回最终停留的位置
+        return CGPoint(x: proposedContentOffset.x + adjustOffsetX, y: proposedContentOffset.y)
+    }
+}
+
